a81da84f78a6ab8b6b5f3df76ab9d138b2a16fe0
Koffi Eli KPONBLANOU <eliscolatour@gmail.com>
2019-11-08 19:09:34 +0100

    Adding Anchor Tags To The Product List HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { Routes, RouterModule } from '@angular/router';
 const appRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
   { path: 'product/:id', component: ProductPageComponent }
+  { path: '', redirectTo: 'products', pathMatch:'full' }
 ];
 
 @NgModule({
