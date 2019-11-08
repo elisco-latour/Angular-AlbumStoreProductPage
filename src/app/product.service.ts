555c79f08e795d2f35e015ceb8bdd96def0f0ca4
Koffi Eli KPONBLANOU <eliscolatour@gmail.com>
2019-11-08 18:38:29 +0100

    Displaying The ProductListComponent

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -24,7 +24,7 @@ export class ProductService {
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(_productsUrl).map((response) => <Product[]>response.json());
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 
 }
