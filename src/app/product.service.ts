e139035713e6bcfaf708622df81da1552aef2434
Koffi Eli KPONBLANOU <eliscolatour@gmail.com>
2019-11-08 14:05:16 +0100

    Add An albumUrl Property

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,6 +9,8 @@ import { Http, Response } from '@angular/http';
 @Injectable()
 export class ProductService {
 
+  _album_url = "../assets/album.json";
+
   constructor( private _http: Http) { }
 
 }
